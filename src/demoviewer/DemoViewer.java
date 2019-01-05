1bce8d4cc86e5e5d53da44c8c83de387cf6cec87
Gena Koganovich <genakoganovich@gmail.com>
2019-01-05 17:27:34 +0200

    0007: adds shades

--- src/demoviewer/DemoViewer.java
+++ src/demoviewer/DemoViewer.java
@@ -82,6 +82,19 @@ public class DemoViewer {
                     v3.x += getWidth() / 2;
                     v3.y += getHeight() / 2;
 
+                    Vertex ab = new Vertex(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
+                    Vertex ac = new Vertex(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z);
+                    Vertex norm = new Vertex(ab.y * ac.z - ab.z * ac.y,
+                            ab.z * ac.x - ab.x * ac.z,
+                            ab.x * ac.y - ab.y * ac.x);
+
+
+                    double normalLength = Math.sqrt(norm.x * norm.x + norm.y * norm.y + norm.z * norm.z);
+                    norm.x /= normalLength;
+                    norm.y /= normalLength;
+                    norm.z /= normalLength;
+                    double angleCos = Math.abs(norm.z);
+
                     // compute rectangular bounds for triangle
                     int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
                     int maxX = (int) Math.min(img.getWidth() - 1,
@@ -104,7 +117,7 @@ public class DemoViewer {
                             double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
                             int zIndex = y * img.getWidth() + x;
                             if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1 && zBuffer[zIndex] < depth) {
-                                img.setRGB(x, y, t.color.getRGB());
+                                img.setRGB(x, y, getShade(t.color, angleCos).getRGB() );
                                 zBuffer[zIndex] = depth;
                             }
                         }
@@ -114,6 +127,18 @@ public class DemoViewer {
 
                 g2.drawImage(img, 0, 0, null);
             }
+            Color getShade(Color color, double shade) {
+                double redLinear = Math.pow(color.getRed(), 2.4) * shade;
+                double greenLinear = Math.pow(color.getGreen(), 2.4) * shade;
+                double blueLinear = Math.pow(color.getBlue(), 2.4) * shade;
+
+                int red = (int) Math.pow(redLinear, 1/2.4);
+                int green = (int) Math.pow(greenLinear, 1/2.4);
+                int blue = (int) Math.pow(blueLinear, 1/2.4);
+
+                return new Color(red, green, blue);
+            }
+
         };
         pane.add(renderPanel, BorderLayout.CENTER);
 
