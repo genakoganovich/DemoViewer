0f719d7cebaed4c60e25df7ad421918a65807860
Gena Koganovich <genakoganovich@gmail.com>
2019-01-05 17:03:26 +0200

    0006: adds z-bufferring

--- src/demoviewer/DemoViewer.java
+++ src/demoviewer/DemoViewer.java
@@ -63,7 +63,11 @@ public class DemoViewer {
 
                 BufferedImage img =
                         new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
-
+                double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+                // initialize array with extremely far away depths
+                for (int q = 0; q < zBuffer.length; q++) {
+                    zBuffer[q] = Double.NEGATIVE_INFINITY;
+                }
                 for (Triangle t : tris) {
                     Vertex v1 = transform.transform(t.v1);
                     Vertex v2 = transform.transform(t.v2);
@@ -97,8 +101,11 @@ public class DemoViewer {
                                     ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
                             double b3 =
                                     ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
-                            if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                            double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+                            int zIndex = y * img.getWidth() + x;
+                            if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1 && zBuffer[zIndex] < depth) {
                                 img.setRGB(x, y, t.color.getRGB());
+                                zBuffer[zIndex] = depth;
                             }
                         }
                     }
@@ -141,7 +148,7 @@ class Triangle {
     }
 }
 class Matrix3 {
-    double[] values;
+    private double[] values;
     Matrix3(double[] values) {
         this.values = values;
     }
